ec24b84b73ec946fd1f44ac5ffd887a042bec4f4
vivienne <viv@yellingviv.com>
2019-12-08 22:58:55 -0800

    create module to generate random text times during specified windows

---
+++ sched.py
@@ -0,0 +1,25 @@
+import random
+import time
+
+sched = {'am': {'lower': 10, 'upper': 11, 'today': ''},
+        'pm': {'lower': 14, 'upper': 17, 'today': ''},
+        'eod': {'lower': 17, 'upper': 18, 'today': ''},
+        'lunch': {'lower': 12, 'upper': 12, 'today': ''}}
+
+def get_daily_sched():
+    """generate a daily schedule of randomized time windows within constraints"""
+
+    for window in sched:
+        sched[window]['today'] = get_time(sched[window]['lower'], sched[window]['upper'])
+
+    return sched
+
+
+def get_time(lower, upper):
+    """generates random time between lower and upper bounds and returns time object"""
+
+    hour = random.randint(lower, upper)
+    minute = random.randint(0, 60)
+    go_time = str(hour) + ':' + str(minute)
+
+    return time.strptime(go_time, "%H:%M")
